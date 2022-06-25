31709bafb782de73f3e98ab43412f01d0988b743
John Jackson <jjackson@nexient.com>
2022-06-25 10:39:05 -0400

    fix:Port issues

--- server.ts
+++ server.ts
@@ -1,23 +1,20 @@
 import express, { Request, Response, Application } from 'express';
 import bodyParser from 'body-parser';
 import { connect } from 'mongoose';
-import routes from './src/routes';
+import routes from "./src/server/routes";
 
 const app: Application = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => 
-  res.send('Root of project')
-);
+app.get("/", (req: Request, res: Response) => res.send("Root of project"));
 
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server is running on url localhost:${8080}`);
+  console.log(`Server is running on url localhost:${port}`);
 });
 
 const db = 'mongodb://localhost:27017/test';
