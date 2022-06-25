0280a7bd4009c103dd21bd8ad246dd2951ee60f8
John Jackson <jjackson@nexient.com>
2022-06-25 09:22:54 -0400

    feat:Add email validation

--- src/controllers/user.test.ts
+++ src/controllers/user.test.ts
@@ -1,18 +1,50 @@
-import { createUser } from './user';
+import { createUser } from "./user";
 
-describe('User', () => {
-  describe('Create', () => {
-    it('creates', async () => {
+describe("User", () => {
+  describe("Create", () => {
+    it("creates", async () => {
       const newUser = {
         firstName: "fakeFirstName",
-        lastName: "fakeLastName", 
-        email: 'fakeEmail@example.com', 
-        password: 'password123'
-      }
+        lastName: "fakeLastName",
+        email: "fakeEmail@example.com",
+        password: "password123",
+      };
 
       const { email } = await createUser(newUser);
 
-      expect(email).toEqual('fakeemail@example.com')
+      expect(email).toEqual(newUser.email.toLocaleLowerCase());
+    });
+
+    describe("Email validation", () => {
+      const testEmailValidation = async (invalidEmail: string) => {
+        const newUser = {
+          firstName: "fakeFirstName",
+          lastName: "fakeLastName",
+          email: invalidEmail,
+          password: "password123",
+        };
+
+        let message: string = "";
+
+        try {
+          await createUser(newUser);
+        } catch (e) {
+          message = e.message;
+        }
+
+        expect(message).toEqual(
+          "User validation failed: email: Please enter a valid email address"
+        );
+      };
+
+      it("only accepts a valid email address", async () => {
+        await testEmailValidation("with a space@example.com");
+        await testEmailValidation("noATSymbol.com");
+        await testEmailValidation("abc.def@mail.c");
+        await testEmailValidation("abc.def@mail#archive.com");
+        await testEmailValidation("abc.def@mail");
+        await testEmailValidation("abc.def@mail..com");
+      });
     });
   });
 });
--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -24,6 +24,8 @@ const userSchema = new Schema<User>({
     required: true,
     unique: true,
     lowercase: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+      'Please enter a valid email address']
   },
   password: {
     type: String,
